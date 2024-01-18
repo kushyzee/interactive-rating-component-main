c5a0aa6aae17c06994e2c543f0c2f06a96ba34d8
kushyzee <niklaus.oa@gmail.com>
2024-01-18 13:49:50 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const ratingsBtnGroup = document.getElementById("rating-btns");
+const ratingBtns = ratingsBtnGroup.children;
+const submitBtn = document.getElementById("submit-btn");
+const ratingResult = document.getElementById("rating-result");
+const mainCard = document.getElementById("main");
+const thankYouCard = document.getElementById("thank-you");
+const spinner = document.getElementById("spinner");
+
+let ratingNum = "";
+
+// FUNCTIONS
+const ratings = (e) => {
+  ratingNum = e.target.innerText;
+};
+
+const submitRating = () => {
+  if (!ratingNum) {
+    alert("please pick a rating");
+    return;
+  }
+
+  ratingResult.innerText = ratingNum;
+  spinner.classList.toggle("v-hidden");
+  thankYouCard.style.display = "initial";
+	setTimeout(() => {
+    mainCard.classList.toggle("hidden2");
+		
+	}, 800);
+	
+	setTimeout(() => {
+    mainCard.style.display = "none";
+    thankYouCard.classList.toggle("hidden");
+  }, 1000);
+};
+
+ratingsBtnGroup.addEventListener("click", ratings);
+submitBtn.addEventListener("click", submitRating);
+console.log(ratingResult.innerText);
